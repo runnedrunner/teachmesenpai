19ca9c258d2296591d70f6f0e247d31bbc7d0c86
runnedrunner <auszhou@nuevaschool.org>
2016-10-10 09:25:08 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-  $( "#stuff" ).click(function() {
-    console.log("stuff");
-  });
-});
+function whenClicked() {
+  console.log("function run")
+  return "1111"
+}
 
+var a = whenClicked()
+var b = whenClicked
 
+console.log(a.toString())
+console.log(b.toString())
 
 
